6e353bdd4d44c109bc5d505f123fff62657a9daf
Amit B Karande <amit.b.karande@boeing.com>
2019-06-02 13:00:20 +0530

    added test case for list with only one element; test fails

--- src/test/java/com/mycomp/ex/tdd/SortTest.java
+++ src/test/java/com/mycomp/ex/tdd/SortTest.java
@@ -15,13 +15,14 @@ public class SortTest {
     @Test
     public void sortings() throws Exception {
         assertThat(sort(intList()), is(intList()));
+        assertThat(sort(intList(1)), is(intList(1)));
     }
 
     private List<Integer> sort(List<Integer> list) {
         return new ArrayList<>();
     }
 
-    private List<Integer> intList() {
-        return Arrays.asList();
+    private List<Integer> intList(Integer... ints) {
+        return Arrays.asList(ints);
     }
 }
