90acf834a8cc4bd73f6f7bf79edbe3416d4da6fd
Amit B Karande <amit.b.karande@boeing.com>
2019-06-02 12:51:41 +0530

    Basic test case for empty list, this is with null tranformation; test fails

--- src/test/java/com/mycomp/ex/tdd/SortTest.java
+++ src/test/java/com/mycomp/ex/tdd/SortTest.java
@@ -1,11 +1,26 @@
 package com.mycomp.ex.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class SortTest {
 
     @Test
-    public void doNothing() throws Exception {
-        
+    public void sortings() throws Exception {
+        assertThat(sort(intList()), is(intList()));
+    }
+
+    private List<Integer> sort(List<Integer> list) {
+        return null;
+    }
+
+    private List<Integer> intList() {
+        return Arrays.asList();
     }
 }
